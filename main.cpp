42c6f0646fb189cf2b76760e90bb8133931d6225
tonystratum <str4tum@gmail.com>
2022-04-09 21:23:39 +0200

    more floats in file; remove some lines; clang-tidify

--- main.cpp
+++ main.cpp
@@ -19,18 +19,13 @@ std::vector<Point> load_from_bin(const char* path) {
 
     if (pcl_stream.is_open()) {
         const size_t flength = pcl_stream.tellg();
-        pcl_stream.seekg(pcl_stream.beg);
+        pcl_stream.seekg(std::ifstream::beg);
 
-        const size_t n_floats = flength / sizeof(char*);
+        const size_t n_floats = flength / FLOAT_SIZE;
         const size_t n_points = n_floats / 3; // 3 coords in Point
-        std::cout << flength << ' '
-                  << n_floats << ' '
-                  << n_points << '\n';
-
         points.reserve(n_points);
 
         char* buf = new char [FLOAT_SIZE];
-
         for (size_t i = 0; i < n_points; ++i) {
             std::array<float, 3> point = {0., 0., 0.};
             for (auto& item : point) {
@@ -51,13 +46,12 @@ std::vector<Point> load_from_bin(const char* path) {
 }
 
 int main() {
-    const std::vector<Point> points = load_from_bin("/home/stratum/CLionProjects/md_task_pcl/car.bin");
-    std::cout << points.empty() << '\n';
+    const std::vector<Point> points = load_from_bin(
+            "/home/stratum/CLionProjects/md_task_pcl/car.bin");
     for (const Point& p : points) {
         std::cout << p.x << ' '
                   << p.y << ' '
                   << p.z << '\n';
     }
-
     return 0;
 }
