563bb8fdab74e3fa1f542447517bf1a08f821037
tonystratum <str4tum@gmail.com>
2022-04-10 10:02:28 +0200

    translate the cloud with centroid

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <array>
 #include <chrono>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <thread>
 
+#include "pcl/common/centroid.h"
+#include "pcl/common/transforms.h"
 #include "pcl/impl/point_types.hpp"
 #include "pcl/point_cloud.h"
 #include "pcl/visualization/pcl_visualizer.h"
@@ -13,28 +15,28 @@
 
 
 pcl::PointCloud<pcl::PointXYZ> load_from_bin(const char *path) {
-    pcl::PointCloud<pcl::PointXYZ> points;
-    std::ifstream pcl_stream;
-    pcl_stream.open(path,
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    std::ifstream pcl_file;
+    pcl_file.open(path,
                     std::ifstream::in | std::ifstream::binary | std::ifstream::ate);
 
-    if (pcl_stream.is_open()) {
-        const size_t flength = pcl_stream.tellg();
-        pcl_stream.seekg(std::ifstream::beg);
+    if (pcl_file.is_open()) {
+        const size_t flength = pcl_file.tellg();
+        pcl_file.seekg(std::ifstream::beg);
 
         const size_t n_floats = flength / FLOAT_SIZE;
         const size_t n_points = n_floats / 3; // 3 coords in Point
-        points.reserve(n_points);
+        cloud.reserve(n_points);
 
         char *buf = new char[FLOAT_SIZE];
         for (size_t i = 0; i < n_points; ++i) {
             std::array<float, 3> point = {0., 0., 0.};
             for (auto &item: point) {
-                pcl_stream.read(buf, FLOAT_SIZE);
+                pcl_file.read(buf, FLOAT_SIZE);
                 item = *(reinterpret_cast<float *>(buf));
             }
 
-            points.emplace_back(
+            cloud.emplace_back(
                     point[0],
                     point[1],
                     point[2]
@@ -42,20 +44,28 @@ pcl::PointCloud<pcl::PointXYZ> load_from_bin(const char *path) {
         }
         delete[] buf;
     }
-    pcl_stream.close();
-    return points;
+    pcl_file.close();
+    return cloud;
 }
 
 int main() {
-    const auto points = load_from_bin(
+    const auto cloud = load_from_bin(
             "/home/stratum/CLionProjects/md_task_pcl/car.bin");
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
-    *basic_cloud_ptr = points;
+    Eigen::Vector4f centroid;
+    pcl::compute3DCentroid(cloud, centroid);
+
+    Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+    transform.translation() << -centroid[0], -centroid[1], -centroid[2];
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+    *cloud_ptr = cloud;
+
+    pcl::transformPointCloud(*cloud_ptr, *cloud_ptr, transform);
 
     pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
     viewer->setBackgroundColor(0, 0, 0);
-    viewer->addPointCloud(basic_cloud_ptr, "cloud");
+    viewer->addPointCloud(cloud_ptr);
     viewer->addCoordinateSystem(1.0);
     viewer->initCameraParameters();
 
